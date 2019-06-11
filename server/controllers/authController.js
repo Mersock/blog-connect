77a3d7f1d91ffc85d6bfdbfa9f106be41b2d518b
Phumthawan <knzphumthawan@gmail.com>
2019-06-12 03:49:02 +0700

    add auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,38 @@
-exports.validateSignup = () => {};
+exports.validateSignup = (req, res, next) => {
+    req.sanitizeBody('name');
+    req.sanitizeBody('email');
+    req.sanitizeBody('password');
 
-exports.signup = () => {};
+    //name is not null and 4-10 characters
+    req.checkBody('name', 'Enter a Name').notEmpty();
+    req.checkBody('name', 'Name  must be between 4 and 10 characters')
+        .isLength({min: 4, max: 10});
 
-exports.signin = () => {};
+    //Email is not null, valid ,and normalized
+    req.checkBody('email','Enter a valid Email')
+        .isEmail()
+        .normalizeEmail();
 
-exports.signout = () => {};
+    //password not null and 4-10 characters
+    req.checkBody('password', 'Enter a Password').notEmpty();
+    req.checkBody('password', 'Password  must be between 4 and 10 characters')
+        .isLength({min: 4, max: 10});
 
-exports.checkAuth = () => {};
+    const errors = req.validationErrors();
+    if(errors){
+       const firstError = errors.map(error => error.msg)[0];
+       return res.status().send(firstError);
+    }
+};
+
+exports.signup = () => {
+};
+
+exports.signin = () => {
+};
+
+exports.signout = () => {
+};
+
+exports.checkAuth = () => {
+};
