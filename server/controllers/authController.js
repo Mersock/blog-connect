7e033cd5239918c5adec7fe38add46239f53158a
Phumthawan <knzphumthawan@gmail.com>
2019-06-12 04:57:41 +0700

    set response

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
 exports.validateSignup = (req, res, next) => {
     req.sanitizeBody('name');
     req.sanitizeBody('email');
@@ -9,7 +12,7 @@ exports.validateSignup = (req, res, next) => {
         .isLength({min: 4, max: 10});
 
     //Email is not null, valid ,and normalized
-    req.checkBody('email','Enter a valid Email')
+    req.checkBody('email', 'Enter a valid Email')
         .isEmail()
         .normalizeEmail();
 
@@ -19,13 +22,32 @@ exports.validateSignup = (req, res, next) => {
         .isLength({min: 4, max: 10});
 
     const errors = req.validationErrors();
-    if(errors){
-       const firstError = errors.map(error => error.msg)[0];
-       return res.status().send(firstError);
+    if (errors) {
+        const firstError = errors.map(error => error.msg)[0];
+        return res.status(400).send({
+            'errors': {
+                'status_code': 400,
+                'message': firstError
+            }
+        });
     }
+    next();
 };
 
-exports.signup = () => {
+exports.signup = async (req, res) => {
+    const {name, email, password} = req.body;
+    const user = await new User({name, email, password});
+    await User.register(user, password, (err, user) => {
+        if (err) {
+            return res.status(500).send({
+                'errors': {
+                    'status_code': 500,
+                    'message': err.message
+                }
+            });
+        }
+         res.json(user);
+    })
 };
 
 exports.signin = () => {
