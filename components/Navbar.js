56257e9215c9e8d4803014c9534b7353e75859e4
Phumthawan <knzphumthawan@gmail.com>
2019-06-19 03:11:30 +0700

    add nav bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,25 +1,35 @@
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import ShareOutlined from "@material-ui/icons/ShareOutlined";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import ShareOutlined from "@material-ui/icons/ShareOutlined";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar = ({classes,router}) => {
+    return (
+        <AppBar className={classes.appBar} position={router.pathname === "/" ? "fixed" : "static"}>
+            <Toolbar>
+                {/*Main title / Home Button*/}
+                <ShareOutlined className={classes.icon}/>
+                <Typography variant="h3" component="h1" className={classes.toolbarTitle}>
+                    Nextconnect
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
 };
 
 const styles = theme => ({
-  appBar: {
-    // z-index 1 higher than the fixed drawer in home page to clip it under the navigation
-    zIndex: theme.zIndex.drawer + 1
-  },
-  toolbarTitle: {
-    flex: 1
-  },
-  icon: {
-    marginRight: theme.spacing.unit
-  }
+    appBar: {
+        // z-index 1 higher than the fixed drawer in home page to clip it under the navigation
+        zIndex: theme.zIndex.drawer + 1
+    },
+    toolbarTitle: {
+        flex: 1
+    },
+    icon: {
+        marginRight: theme.spacing.unit
+    }
 });
 
 export default withStyles(styles)(Navbar);
