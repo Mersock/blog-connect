4f3b871c5c2a47e3241ad864cc3056d919eba972
Phumthawan <knzphumthawan@gmail.com>
2019-06-30 11:09:51 +0700

    Update edit-profile.js
    add edit-profile

--- pages/edit-profile.js
+++ pages/edit-profile.js
@@ -16,198 +16,247 @@ import CloudUpload from "@material-ui/icons/CloudUpload";
 import FaceTwoTone from "@material-ui/icons/FaceTwoTone";
 import EditSharp from "@material-ui/icons/EditSharp";
 import withStyles from "@material-ui/core/styles/withStyles";
-import { authInitialProps } from "../lib/auth";
-import { getAuthUser,updateUser } from "../lib/api";
-
+import {authInitialProps} from "../lib/auth";
+import {getAuthUser, updateUser} from "../lib/api";
+import Link from "./signup";
+import Router from 'next/router';
 
 class EditProfile extends React.Component {
-  state = {
-    _id: "",
-    name: "",
-    email: "",
-    about: "",
-    avatar: "",
-    isLoading: true,
-    avatarPreview: ""
-  };
+    state = {
+        _id: "",
+        name: "",
+        email: "",
+        about: "",
+        avatar: "",
+        isLoading: true,
+        isSaving: false,
+        avatarPreview: "",
+        openSuccess: false,
+        openError: false,
+        error: "",
+        updateUser: null,
+    };
 
-  componentDidMount() {
-    const { auth } = this.props
+    componentDidMount() {
+        const {auth} = this.props
 
-    this.userData = new FormData();
-    getAuthUser(auth.user._id)
-      .then(user => {
-        this.setState({
-          ...user,
-          isLoading: false
-        })
-      }).catch(err => {
-        console.log(err)
-        this.state({ isLoading: false });
-      });
-  }
-
-  handleChange = event => {
-    let inputValue;
-
-    if (event.target.name == "avatar") {
-      inputValue = event.target.files[0]
-      this.setState({ avatarPreview: this.createPreviewImage(inputValue) })
-    } else {
-      inputValue = event.target.value;
+        this.userData = new FormData();
+        getAuthUser(auth.user._id)
+            .then(user => {
+                this.setState({
+                    ...user,
+                    isLoading: false
+                })
+            }).catch(err => {
+            console.log(err)
+            this.state({isLoading: false});
+        });
     }
-    this.userData.set(event.target.name, inputValue);
-    this.setState({ [event.target.name]: inputValue });
-  }
 
-  handleSubmit =  event => {
-      event.preventDefault();
-      updateUser(this.state._id, this.userData).then(data => console.log(data));
-  }
+    handleChange = event => {
+        let inputValue;
 
-  createPreviewImage = file => URL.createObjectURL(file)
+        if (event.target.name == "avatar") {
+            inputValue = event.target.files[0]
+            this.setState({avatarPreview: this.createPreviewImage(inputValue)})
+        } else {
+            inputValue = event.target.value;
+        }
+        this.userData.set(event.target.name, inputValue);
+        this.setState({[event.target.name]: inputValue});
+    }
 
-  render() {
-    const { classes } = this.props;
-    const { name, email, avatar, about, isLoading, avatarPreview } = this.state;
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({isSaving: true});
+        updateUser(this.state._id, this.userData).then(updateUser => {
+            this.setState({updateUser, openSuccess: true});
 
-    return (
-      <div className={classes.root}>
-        <Paper className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <EditSharp />
-          </Avatar>
-          <Typography variant="h5" component="h1">
-            Edit Profile
-          </Typography>
+            setTimeout(() => Router.push({
+                pathname: '/profile',
+                query: { userId: this.state._id },
+            },`/profile/${this.state._id}`), 3000);
+        }).catch(this.showError)
+    };
 
-          <form onSubmit={this.handleSubmit} className={classes.form}>
-            {isLoading ? (
-              <Avatar className={classes.bigAvatar}>
-                <FaceTwoTone />
-              </Avatar>
-            ) : (
-                <Avatar src={avatarPreview || avatar} className={classes.bigAvatar} />
-              )}
-            <input
-              type="file"
-              name="avatar"
-              id="avatar"
-              accept="image/*"
-              onChange={this.handleChange}
-              className={classes.input}
-            />
-            <label htmlFor="avatar" className={classes.uploadButton}>
-              <Button
-                variant="contained"
-                color="secondary"
-                component="span"
-              >
-                Upload Image <CloudUpload />
-              </Button>
-            </label>
-            <span className={classes.filename}>{avatar && avatar.name}</span>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="name">Name</InputLabel>
-              <Input
-                type="text"
-                name="name"
-                value={name}
-                onChange={this.handleChange}
-              />
-            </FormControl>
-            <FormControl margin="normal" fullWidth>
-              <InputLabel htmlFor="about">About</InputLabel>
-              <Input
-                type="text"
-                name="about"
-                value={about}
-                onChange={this.handleChange}
-              />
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="email">Email</InputLabel>
-              <Input
-                type="text"
-                name="email"
-                value={email}
-                onChange={this.handleChange}
-              />
-            </FormControl>
-            <Button
-              type="submit"
-              fullWidth
-              disabled={isLoading}
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Save
-              </Button>
-          </form>
+    showError = err => {
+        const error = err.response && err.response.data || err.message;
+        this.setState({error, openError: true, isSaving: false});
+    };
 
-        </Paper>
-      </div>
-    );
-  }
+    createPreviewImage = file => URL.createObjectURL(file);
+
+    handleClose = () => this.setState({openError: false});
+
+    render() {
+        const {classes} = this.props;
+        const {name, email, avatar, about, isLoading, avatarPreview, isSaving, updateUser, openSuccess, openError, error} = this.state;
+
+        return (
+            <div className={classes.root}>
+                <Paper className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <EditSharp/>
+                    </Avatar>
+                    <Typography variant="h5" component="h1">
+                        Edit Profile
+                    </Typography>
+
+                    <form onSubmit={this.handleSubmit} className={classes.form}>
+                        {isLoading ? (
+                            <Avatar className={classes.bigAvatar}>
+                                <FaceTwoTone/>
+                            </Avatar>
+                        ) : (
+                            <Avatar src={avatarPreview || avatar} className={classes.bigAvatar}/>
+                        )}
+                        <input
+                            type="file"
+                            name="avatar"
+                            id="avatar"
+                            accept="image/*"
+                            onChange={this.handleChange}
+                            className={classes.input}
+                        />
+                        <label htmlFor="avatar" className={classes.uploadButton}>
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                component="span"
+                            >
+                                Upload Image <CloudUpload/>
+                            </Button>
+                        </label>
+                        <span className={classes.filename}>{avatar && avatar.name}</span>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="name">Name</InputLabel>
+                            <Input
+                                type="text"
+                                name="name"
+                                value={name}
+                                onChange={this.handleChange}
+                            />
+                        </FormControl>
+                        <FormControl margin="normal" fullWidth>
+                            <InputLabel htmlFor="about">About</InputLabel>
+                            <Input
+                                type="text"
+                                name="about"
+                                value={about}
+                                onChange={this.handleChange}
+                            />
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="email">Email</InputLabel>
+                            <Input
+                                type="text"
+                                name="email"
+                                value={email}
+                                onChange={this.handleChange}
+                            />
+                        </FormControl>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            disabled={isLoading || isLoading}
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            {isSaving ? "Saving..." : "Save"}
+                        </Button>
+                    </form>
+
+                    {error.errors && <Snackbar
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right'
+                        }}
+                        open={openError}
+                        onClose={this.handleClose}
+                        autoHideDuration={6000}
+                        message={<span className={classes.snack}>{error.errors.message}</span>}
+                    />}
+
+                    {/*success*/}
+                    <Dialog
+                        open={openSuccess}
+                        disableBackdropClick={false}
+                    >
+                        <DialogTitle>
+                            <VerifiedUserTwoTone className={classes.icon}/>
+                            Profile Updated
+                        </DialogTitle>
+
+                        <DialogContent>
+                            <DialogContentText>
+                                User {updateUser && updateUser.name} successfully updated!
+                            </DialogContentText>
+                        </DialogContent>
+                    </Dialog>
+
+                </Paper>
+            </div>
+        );
+    }
 }
 
 const styles = theme => ({
-  root: {
-    width: "auto",
-    display: "block",
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up("md")]: {
-      width: 400,
-      marginLeft: "auto",
-      marginRight: "auto"
+    root: {
+        width: "auto",
+        display: "block",
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up("md")]: {
+            width: 400,
+            marginLeft: "auto",
+            marginRight: "auto"
+        }
+    },
+    bigAvatar: {
+        width: 60,
+        height: 60,
+        margin: "auto"
+    },
+    uploadButton: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "0.25em"
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: theme.spacing.unit * 2
+    },
+    signinLink: {
+        textDecoration: "none",
+        color: "white"
+    },
+    avatar: {
+        margin: theme.spacing.unit,
+        backgroundColor: theme.palette.secondary.main
+    },
+    form: {
+        width: "100%",
+        marginTop: theme.spacing.unit
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 2
+    },
+    snack: {
+        color: theme.palette.secondary.light
+    },
+    icon: {
+        padding: "0px 2px 2px 0px",
+        verticalAlign: "middle",
+        color: "green"
+    },
+    input: {
+        display: "none"
     }
-  },
-  bigAvatar: {
-    width: 60,
-    height: 60,
-    margin: "auto"
-  },
-  uploadButton: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    margin: "0.25em"
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: theme.spacing.unit * 2
-  },
-  signinLink: {
-    textDecoration: "none",
-    color: "white"
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: "100%",
-    marginTop: theme.spacing.unit
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 2
-  },
-  snack: {
-    color: theme.palette.secondary.light
-  },
-  icon: {
-    padding: "0px 2px 2px 0px",
-    verticalAlign: "middle",
-    color: "green"
-  },
-  input: {
-    display: "none"
-  }
 });
 
 EditProfile.getInitialProps = authInitialProps(true);
