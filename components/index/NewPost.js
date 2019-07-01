89894f54bd01d9a95b40e0e93258c9494f3624e9
Phumthawan <knzphumthawan@gmail.com>
2019-07-02 03:03:27 +0700

    add post feed

--- components/index/NewPost.js
+++ components/index/NewPost.js
@@ -1,32 +1,86 @@
-// import Card from "@material-ui/core/Card";
-// import Typography from "@material-ui/core/Typography";
-// import CardHeader from "@material-ui/core/CardHeader";
-// import CardContent from "@material-ui/core/CardContent";
-// import CardActions from "@material-ui/core/CardActions";
-// import Button from "@material-ui/core/Button";
-// import TextField from "@material-ui/core/TextField";
-// import Avatar from "@material-ui/core/Avatar";
-// import IconButton from "@material-ui/core/IconButton";
-// import AddAPhoto from "@material-ui/icons/AddAPhoto";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import AddAPhoto from "@material-ui/icons/AddAPhoto";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-const NewPost = () => <div>NewPost</div>;
+const NewPost = ({classes, auth, text, image, handleChange}) => (
+    <Card className={classes.card}>
+        <CardHeader
+            avatar={<Avatar src={auth.user.avatar}/>}
+            title={<Typography variant="h6" component="h2" >
+                {auth.user.name}
+            </Typography>}
+            className={classes.cardHeader}
+        />
+        <CardContent className={classes.cardContent}>
+            <TextField
+                label="Add a status"
+                value={text}
+                name="text"
+                multiline={true}
+                row="2"
+                placeholder={`What's on your mind, ${auth.user.name}?`}
+                fullWidth={true}
+                margin="normal"
+                onChange={handleChange}
+                variant="outlined"
+                InputLabelProps={{
+                    shrink: true
+                }}
+            />
+            <input
+                accept="image/*"
+                name="image"
+                id="image"
+                onChange={handleChange}
+                className={classes.input}
+                type="file"
+            />
+            <label htmlFor="image">
+                <IconButton
+                    color="secondary"
+                    componenet="span"
+                >
+                    <AddAPhoto/>
+                </IconButton>
+            </label>
+            <span>{image && image.name}</span>
+        </CardContent>
+        <CardActions className={classes.cardActions}>
+            <Button
+                color="primary"
+                variant="contained"
+                disabled={!text}
+                className={classes.submit}
+            >
+                Post
+            </Button>
+        </CardActions>
+    </Card>
+);
 
 const styles = theme => ({
-  card: {
-    marginBottom: theme.spacing.unit * 3,
-    backgroundColor: theme.palette.primary.light
-  },
-  cardContent: {
-    backgroundColor: "white"
-  },
-  input: {
-    display: "none"
-  },
-  cardActions: {
-    display: "flex",
-    flexDirection: "row-reverse"
-  }
+    card: {
+        marginBottom: theme.spacing.unit * 3,
+        backgroundColor: theme.palette.primary.light
+    },
+    cardContent: {
+        backgroundColor: "white"
+    },
+    input: {
+        display: "none"
+    },
+    cardActions: {
+        display: "flex",
+        flexDirection: "row-reverse"
+    }
 });
 
 export default withStyles(styles)(NewPost);
--- components/index/PostFeed.js
+++ components/index/PostFeed.js
@@ -1,21 +1,53 @@
-// import Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
+import Post from './Post';
+import NewPost from "./NewPost";
 
 class PostFeed extends React.Component {
-  state = {};
+    state = {
+        posts: [],
+        text: "",
+        image: ""
+    };
 
-  render() {
-    return <div>PostFeed</div>;
-  }
+    handleChange = () => {
+
+    };
+
+    render() {
+        const {classes, auth} = this.props;
+        const {text, image} = this.state;
+
+        return (
+            <div className={classes.root}>
+                <Typography
+                    variant="h4"
+                    component="h1"
+                    align="center"
+                    color="primary"
+                    className={classes.title}
+                >
+                    Post Feed
+
+                    <NewPost
+                        auth={auth}
+                        text={text}
+                        image={image}
+                        handleChange={this.handleChange}
+                    />
+                </Typography>
+            </div>
+        );
+    }
 }
 
 const styles = theme => ({
-  root: {
-    paddingBottom: theme.spacing.unit * 2
-  },
-  title: {
-    padding: theme.spacing.unit * 2
-  }
+    root: {
+        paddingBottom: theme.spacing.unit * 2
+    },
+    title: {
+        padding: theme.spacing.unit * 2
+    }
 });
 
 export default withStyles(styles)(PostFeed);
